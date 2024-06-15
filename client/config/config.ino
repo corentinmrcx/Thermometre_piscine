76f0bab9b79074755a17b41d8409602655693b61
corentinmrcx <corentinmarcoux51@gmail.com>
2024-06-15 21:09:05 +0200

    Création du fichier de configuration Wifi

---
+++ client/config/config.ino
@@ -0,0 +1,13 @@
+// config.h
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Informations WiFi
+const char* ssid = "TON_SSID";
+const char* password = "TON_MOT_DE_PASSE";
+
+// Adresse IP du serveur ESP8266
+// const char* serverIP = "192.168.1.XX";
+
+#endif
